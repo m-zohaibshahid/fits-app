347429ef057ee16bb0d057ff199364e51b3ec619
ProTechGiant <zohaibali112244@gmail.com>
2023-06-05 17:31:12 +0500

    code pushed

--- src/Screens/AuthScreens/SignUpScreen/index.tsx
+++ src/Screens/AuthScreens/SignUpScreen/index.tsx
@@ -1,83 +1,96 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prettier/prettier */
-import React, { useState } from 'react';
-import { Text, View, TextInput, Modal, ScrollView, Platform } from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  TextInput,
+  Modal,
+  ScrollView,
+  Platform,
+} from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
-import styles from './styles';
-import Toast from 'react-native-toast-message';
-import Button from '../../../Components/Button';
-import Header from '../../../Components/Header/index';
-import { useRegisterUserMutation } from '../../../slice/FitsApi.slice';
-import Colors from '../../../constants/Colors';
-import { ParamListBase, RouteProp, useRoute } from '@react-navigation/native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import styles from "./styles";
+import Toast from "react-native-toast-message";
+import Button from "../../../Components/Button";
+import Header from "../../../Components/Header/index";
+import { useRegisterUserMutation } from "../../../slice/FitsApi.slice";
+import Colors from "../../../constants/Colors";
+import { ParamListBase, RouteProp, useRoute } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const SignUpScreen = ({ navigation }: any) => {
   // Hooks
   const { params }: RouteProp<ParamListBase | any> = useRoute();
   const [hidePass, setHidePass] = useState(true);
-  const [email, setEmail] = useState(''); //abbastrainee1@yopmail.com
-  const [password, setPassword] = useState(''); //Abbas110@
-  const [confirmPassword, setConfirmPassword] = useState(''); //Abbas110@
+  const [email, setEmail] = useState(""); //abbastrainee1@yopmail.com
+  const [password, setPassword] = useState(""); //Abbas110@
+  const [confirmPassword, setConfirmPassword] = useState(""); //Abbas110@
   const [modalVisible, setModalVisible] = useState(false);
 
   const storeUserData = async (userData: any) => {
     try {
-      await AsyncStorage.setItem('userData', JSON.stringify(userData));
-      navigation.navigate('Verification');
+      await AsyncStorage.setItem("userData", JSON.stringify(userData));
+      navigation.navigate("Verification");
     } catch (e) {
       console.log(e);
     }
   };
 
-  const [registerUser, { isLoading, isError, isSuccess }] = useRegisterUserMutation();
+  const [registerUser, { isLoading, isError, isSuccess }] =
+    useRegisterUserMutation();
   const signupCall = async () => {
     if (password.length < 8) {
       Toast.show({
-        type: 'error',
-        text1: 'Password be 8 characters'
+        type: "error",
+        text1: "Password be 8 characters",
       });
     } else if (!password.match(/\W/)) {
       Toast.show({
-        type: 'error',
-        text1: 'Password must contain at least one letter'
+        type: "error",
+        text1: "Password must contain at least one letter",
       });
-    } else if (!email.includes('@')) {
+    } else if (!email.includes("@")) {
       Toast.show({
-        type: 'error',
-        text1: 'Please enter a valid email'
+        type: "error",
+        text1: "Please enter a valid email",
       });
     } else if (password !== confirmPassword) {
       Toast.show({
-        type: 'error',
-        text1: 'Password does not match.'
+        type: "error",
+        text1: "Password does not match.",
       });
     } else {
-      registerUser({ role: params?.role ?? 'trainee', email, password })
+      registerUser({ role: params?.role ?? "trainee", email, password })
         .unwrap()
         .then((data) => {
-          console.log("data-----",data)
+          console.log("data-----", data);
           // Registration successful, handle the response data
-          if (data.message === 'success' || isSuccess) {
+          if (data.message === "success" || isSuccess) {
             Toast.show({
-              type: 'success',
-              text1: 'OTP sent to your email'
+              type: "success",
+              text1: "OTP sent to your email",
             });
-            navigation.navigate('Verification');
+            navigation.navigate("Verification");
             storeUserData(data.data);
           }
         })
         .catch((error) => {
           // Registration failed, handle the error
-          console.error('Registration error:', error);
+          console.error("Registration error:", error);
         });
     }
   };
 
   return (
     <View style={styles.mainContainer}>
-      <Header label={"Let's start here"} subLabel={'Fill in your details to begin'} navigation={navigation} doubleHeader={true} />
+      <Header
+        label={"Let's start here"}
+        subLabel={"Fill in your details to begin"}
+        navigation={navigation}
+        doubleHeader={true}
+      />
       <ScrollView showsVerticalScrollIndicator={false}>
         <View style={styles.main}>
           {/*Start*/}
@@ -90,7 +103,7 @@ const SignUpScreen = ({ navigation }: any) => {
                 <TextInput
                   style={styles.inputTypeStyle}
                   // label="Email"
-                  placeholderTextColor={'#afafafe3'}
+                  placeholderTextColor={"#afafafe3"}
                   placeholder="xyz@gmail.com"
                   value={email}
                   onChangeText={setEmail}
@@ -110,14 +123,19 @@ const SignUpScreen = ({ navigation }: any) => {
                   style={styles.inputTypeStyle}
                   // label="Password"
                   placeholder="**********"
-                  placeholderTextColor={'#afafafe3'}
+                  placeholderTextColor={"#afafafe3"}
                   value={password}
                   onChangeText={setPassword}
                   secureTextEntry={hidePass ? true : false}
                 />
               </View>
               <View style={styles.hideIconView}>
-                <Ionicons name={hidePass ? 'eye-off' : 'eye'} onPress={() => setHidePass(!hidePass)} size={18} color={Colors.white} />
+                <Ionicons
+                  name={hidePass ? "eye-off" : "eye"}
+                  onPress={() => setHidePass(!hidePass)}
+                  size={18}
+                  color={Colors.white}
+                />
               </View>
             </View>
           </View>
@@ -133,26 +151,33 @@ const SignUpScreen = ({ navigation }: any) => {
                   style={styles.inputTypeStyle}
                   // label="Password"
                   placeholder="**********"
-                  placeholderTextColor={'#afafafe3'}
+                  placeholderTextColor={"#afafafe3"}
                   value={confirmPassword}
                   onChangeText={setConfirmPassword}
                   secureTextEntry={hidePass ? true : false}
                 />
               </View>
               <View style={styles.hideIconView}>
-                <Ionicons name={hidePass ? 'eye-off' : 'eye'} onPress={() => setHidePass(!hidePass)} size={18} color={Colors.white} />
+                <Ionicons
+                  name={hidePass ? "eye-off" : "eye"}
+                  onPress={() => setHidePass(!hidePass)}
+                  size={18}
+                  color={Colors.white}
+                />
               </View>
             </View>
           </View>
           {/*end confirm password*/}
         </View>
       </ScrollView>
-      <View style={{ paddingVertical: 20, height: '20%', justifyContent: 'center' }}>
+      <View
+        style={{ paddingVertical: 20, height: "20%", justifyContent: "center" }}
+      >
         <Button
           navigation={navigation}
           // loader={load}
           disabled={!email || !password || !confirmPassword}
-          label={'NEXT'}
+          label={"NEXT"}
           onPress={() => {
             // navigation.navigate('Verification');
             signupCall();
@@ -160,36 +185,40 @@ const SignUpScreen = ({ navigation }: any) => {
         />
       </View>
       {/*Modal Start*/}
-      <Modal
+      {/* <Modal
         animationType="fade"
         transparent={true}
         visible={modalVisible}
         onRequestClose={() => {
           setModalVisible(false);
-        }}>
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <View style={{ width: '90%', alignSelf: 'center' }}>
-              <Text style={styles.vercodetext}>Already Have An Account, Please SignIn!</Text>
-            </View>
+        }}
+      > */}
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+          <View style={{ width: "90%", alignSelf: "center" }}>
+            <Text style={styles.vercodetext}>
+              Already Have An Account, Please SignIn!
+            </Text>
+          </View>
 
-            <View
-              style={{
-                marginTop: Platform.OS === 'ios' ? 50 : 10,
-                width: '100%'
-              }}>
-              <Button
-                navigation={navigation}
-                label={'Go'}
-                onPress={() => {
-                  setModalVisible(false);
-                  navigation.navigate('SignIn');
-                }}
-              />
-            </View>
+          <View
+            style={{
+              marginTop: Platform.OS === "ios" ? 50 : 10,
+              width: "100%",
+            }}
+          >
+            <Button
+              navigation={navigation}
+              label={"Go"}
+              onPress={() => {
+                setModalVisible(false);
+                navigation.navigate("SignIn");
+              }}
+            />
           </View>
         </View>
-      </Modal>
+      </View>
+      {/* </Modal> */}
       {/* Modal End*/}
     </View>
   );
