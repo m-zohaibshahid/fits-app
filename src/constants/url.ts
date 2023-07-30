67a968af6bf86454c03cea8027a72d138270f086
Zohaib Shahid <z-shaid@ejada.com>
2023-07-30 19:18:34 +0500

    conflicts resolved

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,5 +1,6 @@
 // export const url = 'https://fits-backend-git-main-protechgiant.vercel.app/api/';
 // export const url = 'http://192.168.100.114:8080/api';
 // export const url = 'http://192.168.0.100:8080/api';
-export const url = 'http://192.168.0.100:8080/api';
+// export const url = 'http://192.168.0.100:8080/api';
+export const url = 'http://192.168.100.114:8080/api';
 export const SOCKET_URL = 'http://192.168.0.100:8800';
