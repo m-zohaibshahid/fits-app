b3342c1c9f0943b917e47d3c204de93899b74730
Zohaib Shahid <zohaib.shahid@ebryx.com>
2023-08-06 14:33:27 +0500

    Your commit message

--- src/Screens/TraineeScreens/ScheduledClasses.tsx
+++ src/Screens/TraineeScreens/ScheduledClasses.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, StyleSheet, Pressable, ToastAndroid, ActivityIndicator, Platform, Alert } from "react-native";
+import { View, StyleSheet, Pressable, ActivityIndicator, Text } from "react-native";
 import AntDesign from "react-native-vector-icons/AntDesign";
 import { RFValue } from "react-native-responsive-fontsize";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
@@ -7,237 +7,190 @@ import moment from "moment";
 import { NavigationSwitchProp } from "react-navigation";
 import { useSelector } from "react-redux";
 import { UserDetail } from "../../interfaces";
-import { useGetMyBookedClassesQuery } from "../../slice/FitsApi.slice";
+import { useGetMyBookedClassesQuery, useGetUserMeQuery } from "../../slice/FitsApi.slice";
 import Container from "../../Components/Container";
 import Typography from "../../Components/typography/text";
 
-interface PropsInterface {
+interface Props {
   navigation: NavigationSwitchProp;
 }
-const ScheduledClasses = ({ navigation }: PropsInterface) => {
+
+interface MyBookedClass {
+  _id: React.Key | null | undefined;
+  session: {
+    select_date: moment.MomentInput;
+    class_title: string;
+    class_time: string;
+    price: string;
+    details: string;
+  } | null;
+  status: boolean;
+}
+
+const ScheduledClasses = ({ navigation }: Props) => {
   const { userInfo } = useSelector((state: { fitsStore: Partial<UserDetail> }) => state.fitsStore);
+  const { data: userMe } = useGetUserMeQuery({});
 
-  const {data: myBookedClassesApiResponse, refetch, isLoading} = useGetMyBookedClassesQuery(userInfo?.user?._id || "")
+  const { data: myBookedClassesApiResponse, refetch, isLoading } = useGetMyBookedClassesQuery(userMe?.user?._id || "");
 
+  const [expandedClassIndex, setExpandedClassIndex] = useState<number | null>(null);
 
   useEffect(() => {
     navigation.addListener("focus", () => {
-      refetch()
+      refetch();
     });
   }, []);
 
-  const detailsInfoCall = (item: any, i: number) => {
-    let dummy = [...myBookedClassesApiResponse];
-    if (dummy[i].status == true) {
-      dummy.forEach((item) => (item.status = false));
+  const handleDetailsToggle = (index: number) => {
+    if (expandedClassIndex === index) {
+      setExpandedClassIndex(null); // Collapse class details if already expanded
     } else {
-      dummy.forEach((item) => (item.status = false));
-      dummy[i].status = true;
+      setExpandedClassIndex(index); // Expand class details
     }
   };
 
   if (isLoading) {
-    return <ActivityIndicator size="large" color="black" />
+    return <ActivityIndicator size="large" color="black" />;
   }
+
   return (
     <Container>
-            {myBookedClassesApiResponse?.data.booking ? <View style={{display: 'flex', justifyContent: 'center', alignItems: "center", height: '100%'}}><Typography style={{marginBottom: 30}}>---You dont have any Class yet---</Typography></View> : myBookedClassesApiResponse?.data.booking.map((item, i) => (
-              <View style={styles.TopView} key={item._id}>
-                {item.session === null ? null : (
-                  <View style={styles.marchmainview}>
-                    <View style={styles.marchmainview2}>
-                      <View style={{ width: "25%", alignItems: "center" }}>
-                        <Text style={styles.marchtext}>
-                          {moment(item?.session?.select_date).format("DD ")}
-                          {moment(item?.session?.select_date).format("MMMM")}
-                        </Text>
-                        <Text style={styles.Daytext}>({moment(item?.session?.select_date).format("dddd")})</Text>
-                      </View>
-                      <View
-                        style={{
-                          width: "5%",
-                          alignItems: "center",
-                        }}
-                      >
-                        <View
-                          style={{
-                            width: 2,
-                            height: 50,
-                            backgroundColor: "#fff",
-                          }}
-                        ></View>
-                      </View>
-                      <View style={{ width: "35%", flexDirection: "column" }}>
-                        <Text style={styles.marchtext}>{item?.session?.class_title}</Text>
-                        <Text
-                          style={{
-                            color: "#fff",
-                            fontSize: RFValue(8, 580),
-                            fontFamily: "Poppins-Regular",
-                          }}
-                        >
-                          {item?.session?.class_time}
-                        </Text>
-                      </View>
-                      <Pressable
-                        onPress={() => detailsInfoCall(item, i)}
-                        //onPress={() => setDetails(!details)}
-                        style={{
-                          width: "30%",
-                          backgroundColor: "#414143",
-                          alignItems: "center",
-                          borderRadius: 12,
-                          height: 50,
-                          justifyContent: "center",
-                        }}
-                      >
-                        <View
-                          style={{
-                            width: "100%",
-                            alignItems: "center",
-                            flexDirection: "row",
-                          }}
-                        >
-                          <View style={{ width: "80%", justifyContent: "center" }}>
-                            <Text
-                              style={{
-                                color: "#fff",
-                                fontSize: RFValue(14, 580),
-                                fontFamily: "Poppins-Regular",
-                                textAlign: "center",
-                              }}
-                            >
-                              Details
-                            </Text>
-                          </View>
-                          <AntDesign name={item.status ? "up" : "down"} size={15} color={"#fff"} />
-                        </View>
-                      </Pressable>
+      {!myBookedClassesApiResponse?.data.booking ? (
+        <View style={styles.emptyClassesView}>
+          <Typography style={styles.emptyClassesText}>---You don't have any Class yet---</Typography>
+        </View>
+      ) : (
+        myBookedClassesApiResponse?.data.booking.map((item: MyBookedClass, index: number) => (
+          <View style={styles.cardContainer} key={item._id}>
+            {item.session === null ? null : (
+              <View style={[styles.card, { backgroundColor: expandedClassIndex === index ? "#F8F8F8" : "#FFF" }]}>
+                <View style={styles.dateContainer}>
+                  <Text style={styles.dateText}>{moment(item.session.select_date).format("DD MMMM")}</Text>
+                  <Text style={styles.dayText}>({moment(item.session.select_date).format("dddd")})</Text>
+                  <View style={styles.separator} />
+                  <View style={styles.classInfoContainer}>
+                    <Text style={styles.classNameText}>{item.session.class_title}</Text>
+                    <Text style={styles.classTimeText}>{moment(item.session.class_time).format("DD MMMM")}</Text>
+                  </View>
+                  <Pressable onPress={() => handleDetailsToggle(index)} style={[styles.detailsButton, { backgroundColor: expandedClassIndex === index ? "#414143" : "#000" }]}>
+                    <Text style={styles.detailsButtonText}>Details</Text>
+                    <AntDesign name={expandedClassIndex === index ? "up" : "down"} size={15} color={"#fff"} />
+                  </Pressable>
+                </View>
+
+                {expandedClassIndex === index && (
+                  <View style={styles.detailsContainer}>
+                    <View style={styles.detailItem}>
+                      <FontAwesome name="circle" style={styles.dotIcon} />
+                      <Text style={styles.detailText}>
+                        Cost: {"\n"}$ {item.session.price}
+                      </Text>
                     </View>
-                    {/*end Yoga */}
-                    {item.status && (
-                      <View style={{ width: "100%", paddingBottom: 18 }}>
-                        <View style={styles.dotmainview}>
-                          <View style={styles.dotview}>
-                            <FontAwesome name="circle" style={{ color: "#979797" }} />
-                          </View>
-                          <View style={{ width: "90%" }}>
-                            <Text style={styles.textstyle}>
-                              Cost: {"\n"}$ {item?.session?.price}
-                            </Text>
-                          </View>
-                        </View>
-                        <View style={styles.dotmainview}>
-                          <View style={styles.dotview}>
-                            <FontAwesome name="circle" style={{ color: "#979797" }} />
-                          </View>
-                          <View style={{ width: "90%" }}>
-                            <Text style={styles.textstyle}>{item?.session?.details}.</Text>
-                          </View>
-                        </View>
-                        <View style={styles.mainbtnView}>
-                        </View>
-                      </View>
-                    )}
+                    <View style={styles.detailItem}>
+                      <FontAwesome name="circle" style={styles.dotIcon} />
+                      <Text style={styles.detailText}>{item.session.details}.</Text>
+                    </View>
                   </View>
                 )}
               </View>
-            ))}
+            )}
+          </View>
+        ))
+      )}
     </Container>
   );
 };
+
 const styles = StyleSheet.create({
-  fixeheight: {
-    height: 50,
-    justifyContent: "center",
-    borderBottomWidth: 0.5,
-    borderColor: "lightgrey",
+  cardContainer: {
     width: "100%",
     alignItems: "center",
+    marginBottom: 10,
   },
-  fixeheight1: {
-    height: 70,
-    width: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  main: {
-    width: "100%",
-  },
-  footer: {
-    width: "100%",
-    marginBottom: 0,
-    bottom: 0,
-    alignItems: "center",
-    justifyContent: "center",
-    position: "absolute",
-  },
-  TopView: {
-    width: "100%",
-    alignItems: "center",
-  },
-  topView: { width: "90%" },
-  topView1: {
+  card: {
     width: "90%",
-    alignItems: "center",
+    borderRadius: 12,
+    elevation: 2,
+    padding: 16,
   },
-  textstyle: {
-    color: "#ffffff",
-    fontSize: RFValue(12, 580),
-    fontFamily: "Poppins-Regular",
-    marginTop: -5,
-  },
-  Daytext: {
-    color: "#fff",
-    fontSize: RFValue(8, 580),
-    fontFamily: "Poppins-Regular",
-  },
-  dotmainview: {
-    width: "100%",
+  dateContainer: {
     flexDirection: "row",
-  },
-  dotview: {
-    width: "10%",
     alignItems: "center",
   },
-  marchmainview: {
-    width: "90%",
-    backgroundColor: "#000",
-    justifyContent: "center",
-    borderRadius: 14,
-    marginTop: 20,
-  },
-  marchmainview2: {
-    width: "100%",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "row",
-    paddingVertical: 9,
-  },
-  marchtext: {
-    color: "#fff",
-    fontSize: RFValue(10, 580),
+  dateText: {
+    color: "#000",
+    fontSize: RFValue(18),
     fontFamily: "Poppins-SemiBold",
   },
-  mainbtnView: {
-    width: "100%",
+  dayText: {
+    color: "#666",
+    fontSize: RFValue(12),
+    fontFamily: "Poppins-Regular",
+  },
+  separator: {
+    height: 50,
+    width: 2,
+    backgroundColor: "#666",
+    marginHorizontal: 10,
+  },
+  classInfoContainer: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  classNameText: {
+    color: "#000",
+    fontSize: RFValue(18),
+    fontFamily: "Poppins-SemiBold",
+  },
+  classTimeText: {
+    color: "#666",
+    fontSize: RFValue(14),
+    fontFamily: "Poppins-Regular",
+  },
+  detailsButton: {
+    flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
-    borderColor: "#fff",
+    backgroundColor: "#000",
+    borderRadius: 12,
+    height: 50,
+    marginTop: 10,
+  },
+  detailsButtonText: {
+    color: "#fff",
+    fontSize: RFValue(16),
+    fontFamily: "Poppins-Regular",
+    textAlign: "center",
+    marginRight: 10,
+  },
+  detailsContainer: {
     marginTop: 20,
   },
-  ccbtnview: {
-    backgroundColor: "red",
-    width: 140,
-    height: 45,
-    borderRadius: 14,
+  detailItem: {
+    flexDirection: "row",
     alignItems: "center",
-    justifyContent: "center",
+    marginBottom: 10,
   },
-  btntextstyle: {
-    color: "#ffffff",
-    fontSize: RFValue(10, 580),
+  dotIcon: {
+    color: "#979797",
+    fontSize: RFValue(10),
+    marginRight: 10,
+  },
+  detailText: {
+    color: "#666",
+    fontSize: RFValue(14),
+    fontFamily: "Poppins-Regular",
+  },
+  emptyClassesView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  emptyClassesText: {
+    marginBottom: 30,
+    fontSize: RFValue(14),
     fontFamily: "Poppins-Regular",
   },
 });
+
 export default ScheduledClasses;
