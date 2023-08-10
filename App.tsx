0f92355634a55b0d7533373f56b8372931652484
Zohaib Shahid <z-shaid@ejada.com>
2023-08-10 22:22:41 +0500

    ios font issue resolved

--- App.tsx
+++ App.tsx
@@ -3,6 +3,20 @@
 import {Provider} from 'react-redux';
 import Main from './Main';
 import {store} from './src/store/store';
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import AntDesign from "react-native-vector-icons/AntDesign";
+import IonIcon from "react-native-vector-icons/Ionicons";
+import EvilIcons from "react-native-vector-icons/EvilIcons";
+import Entypo from "react-native-vector-icons/Entypo";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import Fontisto from "react-native-vector-icons/Fontisto";
+Icon.loadFont();
+AntDesign.loadFont();
+IonIcon.loadFont();
+EvilIcons.loadFont();
+Entypo.loadFont();
+MaterialCommunityIcons.loadFont();
+Fontisto.loadFont();
 
 export default function App() {
   return (
